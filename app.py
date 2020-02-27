2dca9ee25df97fc55a6dea757f2bc73f09a1bc89
yeongtaeck97s <ljkk1542@naver.com>
2020-02-27 17:40:36 +0900

    update

--- app.py
+++ app.py
@@ -1,23 +1,42 @@
-from flask import Flask, render_template, redirect, request
-from flask import Flask, request, Response
+from flask import render_template
+from flask import Flask
+
 from OpenSSL import SSL
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/main')
 def index():
+    return render_template("main.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/blog')
+def blog():
+    return render_template("blog.html")
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template("portfolio.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/elements')
+def elements():
+    return render_template("elements.html")
+
+@app.route('/')
+def main1():
     return render_template("index.html")
 
-
-@app.route('/corona19')
-def corona19():
-    return render_template("corona19_0225.html")
-
-@app.route('/corona19z')
-def corona192():
-    return render_template("corona19_20200226.html")
-
+@app.route('/index')
+def index2():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
