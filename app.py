f598ec10be22d503ee83d6993b40197ec5c758b4
yeongtaeck97s <ljkk1542@naver.com>
2020-07-06 20:43:58 +0900

    weblog_update
    웹로그 업데이트

--- app.py
+++ app.py
@@ -9,70 +9,9 @@ import os
 import datetime
 import pandas as pd
 import numpy as np
+from flask_matomo import *
 app = Flask(__name__)
-# '''Plotly 예제'app = Flask(__name__)
-# ''
-# import json
-# import plotly
-# import pandas as pd
-# import numpy as np
-#
-# @app.route('/plotly')
-# def plotly():
-#     rng = pd.date_range('1/1/2011', periods=7500, freq='H')
-#     ts = pd.Series(np.random.randn(len(rng)), index=rng)
-#
-#     graphs = [
-#         dict(
-#             data=[
-#                 dict(
-#                     x=[1, 2, 3],
-#                     y=[10, 20, 30],
-#                     type='scatter'
-#                 ),
-#             ],
-#             layout=dict(
-#                 title='first graph'
-#             )
-#         ),
-#
-#         dict(
-#             data=[
-#                 dict(
-#                     x=[1, 3, 5],
-#                     y=[10, 50, 30],
-#                     type='bar'
-#                 ),
-#             ],
-#             layout=dict(
-#                 title='second graph'
-#             )
-#         ),
-#
-#         dict(
-#             data=[
-#                 dict(
-#                     x=ts.index,  # Can use the pandas data structures directly
-#                     y=ts
-#                 )
-#             ]
-#         )
-#     ]
-#
-#     # Add "ids" to each of the graphs to pass up to the client
-#     # for templating
-#     ids = ['graph-{}'.format(i) for i, _ in enumerate(graphs)]
-#
-#     # Convert the figures to JSON
-#     # PlotlyJSONEncoder appropriately converts pandas, datetime, etc
-#     # objects to their JSON equivalents
-#     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-#
-#     return render_template('new_test/plotly.html',
-#                            ids=ids,
-#                            graphJSON=graphJSON)
-#
-# ''' 예제 끝 '''
+matomo = Matomo(app, matomo_url="https://data-cook.matomo.cloud", id_site=1)
 
 ###### TEST #######
 ###### TEST #######
