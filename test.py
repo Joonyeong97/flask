040065f7592c6ba2c00d9ba256a3b1f16e50824a
yeongtaeck97s <ljkk1542@naver.com>
2020-09-03 13:51:31 +0900

    test

--- test.py
+++ test.py
@@ -1,48 +1,55 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
-import re
-from ckonlpy.tag import Twitter
-from selenium.webdriver.chrome.options import Options
-from pyvirtualdisplay import Display
+import datetime
+import pymysql
+current = datetime.datetime.now()
+nine_hour_later = current + datetime.timedelta(hours=9)
+date = nine_hour_later.strftime("%Y-%m-%d %H:%M:%S")
 
-
-display = Display(visible=0, size=(1024, 768))
-display.start()
-
-options = Options()
-options.binary_location = "/usr/bin/google-chrome"
-
-# chrome_options = webdriver.ChromeOptions()
-options.headless = True
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-gpu')
-options.add_argument('--disable-dev-shm-usage')
-
-PATH = 'static/lib/webDriver/chromedriver_linux'
-chrome = webdriver.Chrome(executable_path=PATH, options=options)
-
-#chrome = webdriver.Chrome(executable_path="static/lib/webDriver/chromedriver_linux")
-chrome.implicitly_wait(30)
-
-keyword='마이삭'
-url = 'https://twitter.com/search?q={}&src=typed_query'.format(keyword)
-chrome.get(url)
-chrome.implicitly_wait(30)
-
-body = chrome.find_element_by_css_selector('body')
-#text2 = chrome.find_elements_by_css_selector('#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div:nth-child(2) > div > div > section > div')
-#text2 = chrome.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div/section/div/div')
-
-result = []
-
-for i in range(10):
-    for q in range(2):
-        body.send_keys(Keys.PAGE_DOWN)
-        time.sleep(1)
-    text2 = chrome.find_elements_by_css_selector('#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div:nth-child(2) > div > div > section > div')
-    print(text2.text)
+print(date)
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+# import time
+# import re
+# from ckonlpy.tag import Twitter
+# from selenium.webdriver.chrome.options import Options
+# from pyvirtualdisplay import Display
+#
+#
+# display = Display(visible=0, size=(1024, 768))
+# display.start()
+#
+# options = Options()
+# options.binary_location = "/usr/bin/google-chrome"
+#
+# # chrome_options = webdriver.ChromeOptions()
+# options.headless = True
+# options.add_argument('--headless')
+# options.add_argument('--no-sandbox')
+# options.add_argument('--disable-gpu')
+# options.add_argument('--disable-dev-shm-usage')
+#
+# PATH = 'static/lib/webDriver/chromedriver_linux'
+# chrome = webdriver.Chrome(executable_path=PATH, options=options)
+#
+# #chrome = webdriver.Chrome(executable_path="static/lib/webDriver/chromedriver_linux")
+# chrome.implicitly_wait(30)
+#
+# keyword='마이삭'
+# url = 'https://twitter.com/search?q={}&src=typed_query'.format(keyword)
+# chrome.get(url)
+# chrome.implicitly_wait(30)
+#
+# body = chrome.find_element_by_css_selector('body')
+# #text2 = chrome.find_elements_by_css_selector('#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div:nth-child(2) > div > div > section > div')
+# #text2 = chrome.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div/section/div/div')
+#
+# result = []
+#
+# for i in range(10):
+#     for q in range(2):
+#         body.send_keys(Keys.PAGE_DOWN)
+#         time.sleep(1)
+#     text2 = chrome.find_elements_by_css_selector('#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div:nth-child(2) > div > div > section > div')
+#     print(text2.text)
 #     result.append(re.sub('\n', '', text2[0].text))
 #
 # time.sleep(1)
