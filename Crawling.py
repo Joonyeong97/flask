092f60095df3a7469f435ece8d649c5e2d6c2907
yeongtaeck97s <ljkk1542@naver.com>
2020-09-03 13:18:05 +0900

    test

--- Crawling.py
+++ Crawling.py
@@ -177,22 +177,25 @@ class Crawling:
 
         url = 'https://twitter.com/search?q={}&src=typed_query'.format(keyword)
         chrome.get(url)
-        time.sleep(3)
+        chrome.implicitly_wait(30)
 
         body = chrome.find_element_by_css_selector('body')
         text2 = chrome.find_elements_by_css_selector('#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div:nth-child(2) > div > div > section > div')
         result = []
+
 
         for i in range(10):
             for q in range(3):
                 body.send_keys(Keys.PAGE_DOWN)
                 time.sleep(1)
             for ttt in text2:
-                print(ttt.text)
                 result.append(re.sub('\n', '', ttt.text))
+        print(result)
+
         time.sleep(1)
         if self.platform == 'linux':
             chrome.close()
+
         t = Twitter()
 
         t.add_dictionary(self.sajun(), 'Noun')
@@ -201,6 +204,7 @@ class Crawling:
 
         for i in range(len(result)):
             tokens_ko.append(t.nouns(result[i]))
+
         final = []
         for _, q in enumerate(tokens_ko):
             for i in range(len(q)):
---
+++ test.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import re
+from ckonlpy.tag import Twitter
+
+
+chrome = webdriver.Chrome(executable_path="static/lib/webDriver/chromedriver_linux")
+chrome.implicitly_wait(30)
+
+keyword='마이삭'
+url = 'https://twitter.com/search?q={}&src=typed_query'.format(keyword)
+chrome.get(url)
+chrome.implicitly_wait(30)
+
+body = chrome.find_element_by_css_selector('body')
+#text2 = chrome.find_elements_by_css_selector('#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div:nth-child(2) > div > div > section > div')
+#text2 = chrome.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div/section/div/div')
+
+result = []
+
+for i in range(10):
+    for q in range(2):
+        body.send_keys(Keys.PAGE_DOWN)
+        time.sleep(1)
+    text2 = chrome.find_elements_by_css_selector('#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div:nth-child(2) > div > div > section > div')
+    result.append(re.sub('\n', '', text2[0].text))
+
+time.sleep(1)
+# if self.platform == 'linux':
+#     chrome.close()
+
+t = Twitter()
+
+# t.add_dictionary(self.sajun(), 'Noun')
+print('단어사전 추출완료')
+tokens_ko = []
+
+for i in range(len(result)):
+    tokens_ko.append(t.nouns(result[i]))
+
+final = []
+for _, q in enumerate(tokens_ko):
+    for i in range(len(q)):
+        final.insert(-1, q[i])
+print(final[0])
